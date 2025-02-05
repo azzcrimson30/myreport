d8ab2bce6c017e63c7f95d91ede5e28fd7c407bd
azzcrimson30 <asnawihashim30@gmail.com>
2025-02-05 16:37:45 +0800

    add detail loginpage

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,13 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="{{ asset('css/custom.css') }}" rel="stylesheet"> <!-- Added custom CSS -->
-<main class="login-form">
-    <div class="container">
+<main class="login-form" style="background-color: ivory">
+    <div class="container" style="height: 100%;
+        background-image: url({{asset('img/bgimage.jpg')}});
+        background-repeat:no-repeat;
+        background-position: center;
+        background-size: 100%">
         <div class="row justify-content-center m-3">
-            <h3 class="text-center m-4" style="color: blue">Welcome to Company Management System</h3><hr>
+            <h3 class="text-center m-4" style="color: blue">Welcome to MyReport</h3><hr>
             <div class="col-md-4">
                 <div class="card shadow-lg mt-4"> <!-- Added shadow for depth -->
                     <h4 class="card-header text-center" style="background-color: lightblue">Login</h3>
